70c54941859f30a2f9afada0671b6f9a6c359024
Felix Saint-Genis <saintgenisfelix@gmail.com>
2018-07-04 11:01:57 +0200

    add the ressourceCenter script

--- assembl/static2/tests/e2e/ressourceCenter/ressourceCenter.sh
+++ assembl/static2/tests/e2e/ressourceCenter/ressourceCenter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ../../../node_modules/jest/bin/jest.js ./newRessource.spec.js
 ../../../node_modules/jest/bin/jest.js ./modifyRessource.spec.js
+../../../node_modules/jest/bin/jest.js ./secondRessource.spec.js
 ../../../node_modules/jest/bin/jest.js ./deleteRessource.spec.js
